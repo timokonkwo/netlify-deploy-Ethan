ec979d5b9f3e68d575105031f454165b29b98eea
Ethan Miller <ethan.miller@aa.com>
2024-02-20 19:35:27 -0600

    Add listener to serve application locally
    - Removed preloader opacity due to hidden child elements inheriting it
    - Define default cap and volume in Express route for local testing
    - Serve static files (images) by using the express.static middleware. Images are now located in the public/images directory, and the server is configured to serve them correctly. This resolves the issue of images not loading.

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ const titles = {
   home: "PAAL AI | Create and Integrate Advanced Crypto & AI Bots",
 };
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.render("pages/home", {
     title: titles["home"],
+    cap: 101414052,
+    volume: 20000000
   });
 });
 
@@ -22,4 +26,10 @@ app.get("*", (req, res) => {
   res.redirect("/");
 });
 
+
 module.exports = app;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
